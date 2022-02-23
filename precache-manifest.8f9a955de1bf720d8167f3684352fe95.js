248078676a702c027ed48891535999cd44c2d02f
sshaposhnik <sshaposhnik.sergey88@gmail.com>
2022-02-23 17:44:38 +0700

    Updates

---
+++ precache-manifest.8f9a955de1bf720d8167f3684352fe95.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a1d91cd1f5c16c97d72f40c97b02fd8",
+    "url": "/GDonskoy/flight-schedule/index.html"
+  },
+  {
+    "revision": "791cd2af56fcc57450cc",
+    "url": "/GDonskoy/flight-schedule/static/css/2.2476dcc0.chunk.css"
+  },
+  {
+    "revision": "bad6da0e6cc65a99d90d",
+    "url": "/GDonskoy/flight-schedule/static/css/main.0d029e9c.chunk.css"
+  },
+  {
+    "revision": "791cd2af56fcc57450cc",
+    "url": "/GDonskoy/flight-schedule/static/js/2.db4f5003.chunk.js"
+  },
+  {
+    "revision": "55aece240035d21d1f34d00ace818e3e",
+    "url": "/GDonskoy/flight-schedule/static/js/2.db4f5003.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bad6da0e6cc65a99d90d",
+    "url": "/GDonskoy/flight-schedule/static/js/main.e223dbc8.chunk.js"
+  },
+  {
+    "revision": "e3561346c984e4a6a85a",
+    "url": "/GDonskoy/flight-schedule/static/js/runtime-main.906029a6.js"
+  },
+  {
+    "revision": "6ba609594b31073e0015206d0513cf75",
+    "url": "/GDonskoy/flight-schedule/static/media/china.6ba60959.svg"
+  },
+  {
+    "revision": "81465a1d37c595822d2e0ae687586960",
+    "url": "/GDonskoy/flight-schedule/static/media/france.81465a1d.svg"
+  },
+  {
+    "revision": "90b012ac842e709926e2a9b3c6f72c52",
+    "url": "/GDonskoy/flight-schedule/static/media/india.90b012ac.svg"
+  },
+  {
+    "revision": "cb4efe8f99aff060c5c6b6afecaba4ca",
+    "url": "/GDonskoy/flight-schedule/static/media/iran.cb4efe8f.svg"
+  },
+  {
+    "revision": "aefa133becc557b68541191d53878101",
+    "url": "/GDonskoy/flight-schedule/static/media/japan.aefa133b.svg"
+  },
+  {
+    "revision": "ea9f41eb222f76f07446dc078249144a",
+    "url": "/GDonskoy/flight-schedule/static/media/kazakhstan.ea9f41eb.svg"
+  },
+  {
+    "revision": "082ced438d8adca9cc1df65282aca223",
+    "url": "/GDonskoy/flight-schedule/static/media/new-zealand.082ced43.svg"
+  },
+  {
+    "revision": "1b038aa5baa2d162dd7c6ef4db931a89",
+    "url": "/GDonskoy/flight-schedule/static/media/russia.1b038aa5.svg"
+  },
+  {
+    "revision": "358c32a949bbbc803b4a65750ee18ea2",
+    "url": "/GDonskoy/flight-schedule/static/media/space1.358c32a9.jpg"
+  },
+  {
+    "revision": "dbb2d233dc0dcb8f8dc8b2ac996f282d",
+    "url": "/GDonskoy/flight-schedule/static/media/uk.dbb2d233.svg"
+  },
+  {
+    "revision": "4a90ee18671e922d4eb77cd821ed821b",
+    "url": "/GDonskoy/flight-schedule/static/media/usa.4a90ee18.svg"
+  }
+]);
